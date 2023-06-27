bf9a14fc292abf2504e0c8c1f99b669022418836
Edmundo Marvão <shiripiti@hotmail.com>
2023-06-27 12:56:07 +0100

    Reword project into C#

---
+++ MS2-Launcher/MainWindow.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace MS2_Launcher
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.MinimizeButton = new System.Windows.Forms.Button();
+            this.NewsButton = new System.Windows.Forms.Button();
+            this.GameButton = new System.Windows.Forms.Button();
+            this.Logo = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.TopPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.Black;
+            this.TopPanel.Controls.Add(this.CloseButton);
+            this.TopPanel.Controls.Add(this.MinimizeButton);
+            this.TopPanel.Controls.Add(this.NewsButton);
+            this.TopPanel.Controls.Add(this.GameButton);
+            this.TopPanel.Controls.Add(this.Logo);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(1280, 75);
+            this.TopPanel.TabIndex = 0;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.BackColor = System.Drawing.Color.Black;
+            this.CloseButton.BackgroundImage = global::MS2_Launcher.Properties.Resources.close;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.CloseButton.ForeColor = System.Drawing.Color.Transparent;
+            this.CloseButton.Location = new System.Drawing.Point(1238, 23);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(30, 30);
+            this.CloseButton.TabIndex = 5;
+            this.CloseButton.UseVisualStyleBackColor = false;
+            // 
+            // MinimizeButton
+            // 
+            this.MinimizeButton.BackgroundImage = global::MS2_Launcher.Properties.Resources.minimize;
+            this.MinimizeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.MinimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.MinimizeButton.ForeColor = System.Drawing.Color.Transparent;
+            this.MinimizeButton.Location = new System.Drawing.Point(1202, 23);
+            this.MinimizeButton.Name = "MinimizeButton";
+            this.MinimizeButton.Size = new System.Drawing.Size(30, 30);
+            this.MinimizeButton.TabIndex = 4;
+            this.MinimizeButton.UseVisualStyleBackColor = true;
+            // 
+            // NewsButton
+            // 
+            this.NewsButton.Location = new System.Drawing.Point(227, 23);
+            this.NewsButton.Name = "NewsButton";
+            this.NewsButton.Size = new System.Drawing.Size(75, 30);
+            this.NewsButton.TabIndex = 3;
+            this.NewsButton.Text = "News";
+            this.NewsButton.UseVisualStyleBackColor = true;
+            // 
+            // GameButton
+            // 
+            this.GameButton.Location = new System.Drawing.Point(133, 23);
+            this.GameButton.Name = "GameButton";
+            this.GameButton.Size = new System.Drawing.Size(75, 30);
+            this.GameButton.TabIndex = 2;
+            this.GameButton.Text = "Game";
+            this.GameButton.UseVisualStyleBackColor = true;
+            // 
+            // Logo
+            // 
+            this.Logo.BackgroundImage = global::MS2_Launcher.Properties.Resources.logo;
+            this.Logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Logo.Location = new System.Drawing.Point(12, 12);
+            this.Logo.Name = "Logo";
+            this.Logo.Size = new System.Drawing.Size(100, 50);
+            this.Logo.TabIndex = 2;
+            this.Logo.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 75);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(50, 693);
+            this.panel1.TabIndex = 1;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1280, 768);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.TopPanel);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainWindow";
+            this.TopPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel TopPanel;
+        private Button CloseButton;
+        private Button MinimizeButton;
+        private Button NewsButton;
+        private Button GameButton;
+        private PictureBox Logo;
+        private Panel panel1;
+    }
+}
